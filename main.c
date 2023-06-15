356530b55666bc06e1ffa14b279630c2ef941e8c
Issame Chouaref <ichouare@e2r3p13.1337.ma>
2023-06-15 20:58:08 +0100

    here

--- main.c
+++ main.c
@@ -47,7 +47,7 @@ int key_hook(int key , t_vars *vars)
 		vars->keys->key_A = 1;
 	if(key == 2)
 	{
-		puts("here");
+		//puts("here");
 		vars->keys->key_D = 1;
 	}
 	if(key == 123)
@@ -96,10 +96,10 @@ int ft_steps(t_vars *vars, double angle)
 				vars->dy += stepsy;
 		}	
 	vars->hitH = sqrt(pow(vars->dx - vars->player_x, 2) + pow(vars->dy - vars->player_y, 2));
-	if(vars->hitH >= (14* 64))
-		vars->hitH = 14 * 64; 
-	if(vars->hitH < 0)
-		vars->hitH = 0; 
+	// if(vars->hitH >= (14* 64))
+	// 	vars->hitH = 14 * 64; 
+	// if(vars->hitH < 0)
+	// 	vars->hitH = 0; 
 	return 0;
 }
 
@@ -137,10 +137,10 @@ int ft_stepsV(t_vars *vars, double angle)
 				vars->dyV += stepsy;	
 		}
 	 vars->hitV = sqrt(pow(vars->dxV - vars->player_x, 2) + pow(vars->dyV - vars->player_y, 2));
-	 if(vars->hitV > (30* 64))
-		vars->hitV= 34 * 64; 
-	 if(vars->hitV < 0)
-		vars->hitV = 0; 
+	//  if(vars->hitV > (30* 64))
+	// 	vars->hitV= 34 * 64; 
+	//  if(vars->hitV < 0)
+	// 	vars->hitV = 0; 
 	return 0;
 }
 
@@ -164,84 +164,107 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
-void  *ft_facing(t_vars *vars, double angle, int offsetx, int offsety)
+void * ft_facing(t_vars *vars, double angle, int offsetx, int  offsety)
 {
-	// int color = 0;
-	void *buf = NULL;
-	t_data *img;
+	 //int color = 0;
+		t_data *img;
 	img =  malloc(sizeof(t_data));
- if(vars->hitV > vars->hitH )
+	void *buf = NULL;
+ if(vars->hitV > vars->hitH)
 			{
 				if(sin(angle) < 0)
-				{
-					img->img = vars->texture_E;
-					img->addr = mlx_get_data_addr(img->img,  &img->bits_per_pixel, &img->line_length, &img->endian);
-					buf = img->addr + (offsety * img->line_length + offsetx * (img->bits_per_pixel / 8));
-					// my_mlx_pixel_put(vars->data, x, y, *(unsigned int *)buf);
-					//mlx_pixel_put(vars->mlx, vars->mlx_win,x  ,  y  , create_trgb(0,200,200,100));
-					// color = create_trgb(0,0, 0, 255); // blue
-				
-					puts("here2");
-				}
-				else if(sin(angle) > 0)
-				{
-						img->img = vars->texture_N;
-						img->addr = mlx_get_data_addr(img->img,  &img->bits_per_pixel, &img->line_length, &img->endian);
-						buf = img->addr + (offsety * img->line_length + offsetx * (img->bits_per_pixel / 8));
-						// my_mlx_pixel_put(vars->data, x, y, *(unsigned int *)buf);
-						//mlx_pixel_put(vars->mlx, vars->mlx_win,x  ,  y  , create_trgb(0,250,250,100));
-						//color = create_trgb(0,250,0,0);
-					
-					puts("here4");
-				}
-				
-			}
-		else if(vars->hitV < vars->hitH)
-			{
-				if(cos(angle) < 0)
-				{
-					img->img = vars->texture_E;
-					img->addr = mlx_get_data_addr(img->img,  &img->bits_per_pixel, &img->line_length, &img->endian);
-					buf = img->addr + (offsety * img->line_length + offsetx * (img->bits_per_pixel / 8));
-					// my_mlx_pixel_put(vars->data, x, y, *(unsigned int *)buf);
-					//mlx_pixel_put(vars->mlx, vars->mlx_win,x  ,  y  , create_trgb(0,100,150,150));
-					//color = create_trgb(0,106, 90, 205); // green
-				
-				} 
-				else if(cos(angle) > 0)
 				{
 					img->img = vars->texture_N;
 					img->addr = mlx_get_data_addr(img->img,  &img->bits_per_pixel, &img->line_length, &img->endian);
 					buf = img->addr + (offsety * img->line_length + offsetx * (img->bits_per_pixel / 8));
-					// my_mlx_pixel_put(vars->data, x, y, *(unsigned int *)buf);
-					// mlx_pixel_put(vars->mlx, vars->mlx_win, x  ,  y  , create_trgb(0,100,100,100));
-					 //color = create_trgb(0,255, 165, 0); // orange
-			
-					puts("here1");
+
+				}
+				else
+				{
+						img->img = vars->texture_N;
+						img->addr = mlx_get_data_addr(img->img,  &img->bits_per_pixel, &img->line_length, &img->endian);
+						buf = img->addr + (offsety * img->line_length + offsetx * (img->bits_per_pixel / 8));
+				}
+				
+			}
+		else  
+			{
+				if(cos(angle) < 0)
+				{
+					img->img = vars->texture_N;
+					img->addr = mlx_get_data_addr(img->img,  &img->bits_per_pixel, &img->line_length, &img->endian);
+					buf = img->addr + (offsety * img->line_length + offsetx * (img->bits_per_pixel / 8));
+
+				} 
+				else
+				{
+					img->img = vars->texture_N;
+					img->addr = mlx_get_data_addr(img->img,  &img->bits_per_pixel, &img->line_length, &img->endian);
+					buf = img->addr + (offsety * img->line_length + offsetx * (img->bits_per_pixel / 8));
 				}
 			}
-	free(img);
+	
+	
 	return buf;
+
 }
-void ft_dda(t_vars *vars, double x, double y, double Dx, double Dy,double angle)
+// void ft_dda(t_vars *vars, double x, double y, double Dx, double Dy,t_data *img)
+// {
+// 	int dtx = 0;
+// 	int dty = 0;
+// 	int steps = 0;
+// 	double xinc = 0;
+// 	double yinc = 0;
+// 	void *buf = NULL;
+// 	int offsety = 0;
+// 	int offsetx = 0;	
+// 	double walltopixel = 0;
+
+// 	int m = 1;
+
+// 	dtx = Dx  - x;
+// 	dty = Dy - y;
+// 	if(abs((int)(dtx)) > abs((int)(dty)))
+// 		steps = abs((int)(dtx));
+// 	else
+// 		steps = abs((int)(dty));
+// 	xinc = (double)dtx / (double)steps;
+// 	yinc = (double)dty / (double)steps;
+// 	walltopixel = y;
+// 	// if(walltopixel  < 0)
+// 	// 	walltopixel = 0;
+// 	if(vars->hitH <= vars->hitV)
+// 	{
+// 		offsetx = (fmod(vars->dx , 64)/64.0) * 64;
+// 	}
+// 	else
+// 		offsetx = (fmod(vars->dyV , 64)/64.0) * 64;
+// 	while(m <= steps)
+// 	{
+// 		if(y < 0 || y >= 13*64)
+// 			y = 0;
+// 		offsety = abs((int)(y - walltopixel)) * ((double)(64) / (Dy - walltopixel));
+// 		//unsigned int  color =  *(unsigned int  *)ft_facing(vars, angle, offsetx, offsety);
+// 		buf = img->addr + (offsety * img->line_length + offsetx * (int)(img->bits_per_pixel / 8));
+// 		my_mlx_pixel_put(vars->data,(int)x  , (int)y  ,  *(unsigned int *)buf);
+// 		x += xinc;
+// 		y += yinc;
+// 		m++;
+// 	}
+// }
+
+void ft_floor(t_vars *vars, double x, double y, double Dx, double Dy,int color)
 {
 	int dtx = 0;
 	int dty = 0;
 	int steps = 0;
 	double xinc = 0;
 	double yinc = 0;
-	int offsetx = 0;	
-	int offsety = 0;
-	int walltopixel = 0;
+
+
 	
+
 	int m = 1;
-	// double anglecos = 0;
-	// double anglesin = 0;
-	// void * buf;
-
-
-
-	
 
 	dtx = Dx  - x;
 	dty = Dy - y;
@@ -251,28 +274,60 @@ void ft_dda(t_vars *vars, double x, double y, double Dx, double Dy,double angle)
 		steps = abs((int)(dty));
 	xinc = (double)dtx / (double)steps;
 	yinc = (double)dty / (double)steps;
-	walltopixel = ((14 * 64) / 2) - (vars->wallStripHeight  / 2);
-	if(walltopixel  < 0)
-		walltopixel = 0;
-	if(vars->hitH <= vars->hitV)
-	{
-		offsetx = (int)vars->dx % 64;
-	}
-	else
-		offsetx = (int)vars->dy % 64;
-		
-	
+
 	while(m <= steps)
 	{
-		if(y < 0 || y > (13 * 64))
+		if(y < 0 || y >= 13*64)
 			y = 0;
-			offsety = (vars->dy - walltopixel) * ((double)64 / vars->wallStripHeight);
-			//mlx_pixel_put(vars->mlx, vars->mlx_win,(int)x  , (int)y  , ft_facing(vars, angle, offsetx, offsety));
-			//printf("%d\n", *(unsigned int *)ft_facing(vars, angle, offsetx, offsety) );
+		// offsety = abs((int)(y - walltopixel)) * ((double)(64) / (Dy - walltopixel));
+		// //unsigned int  color =  *(unsigned int  *)ft_facing(vars, angle, offsetx, offsety);
+		// buf = img->addr + (offsety * img->line_length + offsetx * (int)(img->bits_per_pixel / 8));
+		my_mlx_pixel_put(vars->data,(int)x  , (int)y  ,  color);
+		x += xinc;
+		y += yinc;
+		m++;
+	}
+}
 
-			
-			my_mlx_pixel_put(vars->data, x, y, *(unsigned int *)ft_facing(vars, angle, offsetx, offsety));
-			
+void ft_dda(t_vars *vars, double x, double y, double Dx, double Dy,t_data *img)
+{
+	int dtx = 0;
+	int dty = 0;
+	int steps = 0;
+	double xinc = 0;
+	double yinc = 0;
+	void *buf = NULL;
+	int offsety = 0;
+	int offsetx = 0;	
+	double walltopixel = 0;
+
+	int m = 1;
+
+	dtx = Dx  - x;
+	dty = Dy - y;
+	if(abs((int)(dtx)) > abs((int)(dty)))
+		steps = abs((int)(dtx));
+	else
+		steps = abs((int)(dty));
+	xinc = (double)dtx / (double)steps;
+	yinc = (double)dty / (double)steps;
+	walltopixel = y;
+	// if(walltopixel  < 0)
+	// 	walltopixel = 0;
+	if(vars->hitH <= vars->hitV)
+	{
+		offsetx = (fmod(vars->dx , 64)/64.0) * 64;
+	}
+	else
+		offsetx = (fmod(vars->dyV , 64)/64.0) * 64;
+	while(m <= steps)
+	{
+		if(y < 0 || y >= 13*64)
+			y = 0;
+		offsety = abs((int)(y - walltopixel)) * ((double)(64) / (Dy - walltopixel));
+		//unsigned int  color =  *(unsigned int  *)ft_facing(vars, angle, offsetx, offsety);
+		buf = img->addr + (offsety * img->line_length + offsetx * (int)(img->bits_per_pixel / 8));
+		my_mlx_pixel_put(vars->data,(int)x  , (int)y  ,  *(unsigned int *)buf);
 		x += xinc;
 		y += yinc;
 		m++;
@@ -281,32 +336,54 @@ void ft_dda(t_vars *vars, double x, double y, double Dx, double Dy,double angle)
 }
 void draw_wall(t_vars *vars, int i, double angle)
 {
-	
 	double withrayon = 0;
 	double heightrayon = 0;
 	double rayDistance = 0;
+	t_data *img;
+	img =  malloc(sizeof(t_data));
 	
 	if(vars->hitV <= vars->hitH)
 		rayDistance = vars->hitV * cos(angle - vars->player_angle);
 	else
 		rayDistance = vars->hitH * cos(angle - vars->player_angle);
-	double windowWith = 2112;
- double windowHeight = 896;
+	double windowWith = 33*64;
+ double windowHeight = 13*64;
 	double wallheightStrip = 0;
 	double distancetopeoject = (double)(windowWith / 2) / tan(30 * (PI / 180));
 	wallheightStrip = (((double)64 / rayDistance) * distancetopeoject);
 	withrayon = i;
 	heightrayon = (windowHeight / 2 ) - (wallheightStrip / 2);
-	// vars->data->img = mlx_new_image(vars->mlx, (33 * 64), (14 * 64));
-	// vars->data->addr = mlx_get_data_addr(vars->data->img,  &vars->data->bits_per_pixel, &vars->data->line_length, &vars->data->endian);
-	 ft_dda(vars, withrayon, heightrayon, withrayon, (windowHeight / 2 ) + (wallheightStrip / 2),angle );
-	
-	mlx_put_image_to_window(vars->mlx, vars->mlx_win, vars->data->img, 0, 0);
-	//mlx_destroy_image(vars->mlx, vars->data->img);
-	// puts("here");
-	//mlx_destroy_image(vars->mlx, vars->data->img);
-	
-
+	vars->wallStripHeight = wallheightStrip;
+	 ft_floor(vars, i,  0, i, heightrayon , create_trgb(1, 255, 225, 100));
+	if(vars->hitV > vars->hitH)
+			{
+				if(sin(angle) < 0)
+				{
+					img->img = vars->texture_N;
+					img->addr = mlx_get_data_addr(img->img,  &img->bits_per_pixel, &img->line_length, &img->endian);
+				}
+				else
+				{
+						img->img = vars->texture_N;
+						img->addr = mlx_get_data_addr(img->img,  &img->bits_per_pixel, &img->line_length, &img->endian);
+				}
+				
+			}
+		else  
+			{
+				if(cos(angle) < 0)
+				{
+					img->img = vars->texture_N;
+					img->addr = mlx_get_data_addr(img->img,  &img->bits_per_pixel, &img->line_length, &img->endian);
+				} 
+				else
+				{
+					img->img = vars->texture_N;
+					img->addr = mlx_get_data_addr(img->img,  &img->bits_per_pixel, &img->line_length, &img->endian);
+				}
+			}
+				ft_dda(vars, withrayon, heightrayon, withrayon, (windowHeight / 2 ) + (wallheightStrip / 2), img);
+			 ft_floor(vars, i,  heightrayon + wallheightStrip, i,(13*64), create_trgb(5, 255, 225, 255));
 	}
 
 void ft_move(t_vars *vars)
@@ -474,19 +551,10 @@ int ft_draw(t_vars *vars)
 	int j = 0;
 
 	int x = 0;
-	// int y = 0;
 	int column = 0;
-	// int height= 64;
-	// int width= 64;
-	// int k = 0;
-
-	// vars->texture_N = mlx_xpm_file_to_image(vars->mlx,"./path_to_the_north_texture.xpm", &width, &height);
-	// vars->texture_W = mlx_xpm_file_to_image(vars->mlx,"./path_to_the_west_texture.xpm", &width, &height);
-	// vars->texture_E= mlx_xpm_file_to_image(vars->mlx,"./path_to_the_east_texture.xpm", &width, &height);
-	// vars->texture_S= mlx_xpm_file_to_image(vars->mlx,"./path_to_the_south_texture.xpm", &width, &height);
-	
+	vars->data->img = mlx_new_image(vars->mlx, (33 * 64), (14 * 64));
+	vars->data->addr = mlx_get_data_addr(vars->data->img,  &vars->data->bits_per_pixel, &vars->data->line_length, &vars->data->endian);
 	mlx_clear_window(vars->mlx, vars->mlx_win);
-	
 	ft_move(vars);
 	while(vars->store_map[i] != NULL)
 	{
@@ -529,13 +597,12 @@ int ft_draw(t_vars *vars)
 						vars->dy = vars->dyV;
 					}
 					//ft_dda(vars, vars->player_x, vars->player_y, vars->dx, vars->dy, angle);
-					
 			draw_wall(vars, column, angle);
 					
 					
 					vars->hitH = 0;
 					vars->hitV = 0;
-					angle += vars->field_of_view /( (double)vars->num_rays);
+					angle += vars->field_of_view / vars->num_rays;
 				
 					column++;
 					}
@@ -560,6 +627,8 @@ int ft_draw(t_vars *vars)
 		}
 		i++;
 }
+mlx_put_image_to_window(vars->mlx, vars->mlx_win, vars->data->img, 0, 0);
+mlx_destroy_image(vars->mlx, vars->data->img);
 		vars->keys->key_A = 0;
  		vars->keys->key_w = 0;
  		vars->keys->key_S = 0;
@@ -572,45 +641,7 @@ int ft_draw(t_vars *vars)
 
 }
 
-void fill_buffer2(t_vars *vars, t_data *data)
-{
-	int	y;
-	int	x;
-	int	i;
-	t_data texture;
-	void *buf = NULL;
-	vars->buf =  malloc(sizeof(unsigned int )  *4);
-	vars->imgs = malloc(sizeof(char *) * 4);
-	i = 0;
-	while(i <  4)
-	{
-		vars->buf[i] = malloc(sizeof(unsigned int ) * (64 * 64));
-	i++;
-	}
-	i = 0;
-	x = -1;
-	while (i < 4)
-	{
-		// data->img = mlx_new_image(vars->mlx, 64, 64);
-		texture.img = vars->texture_N;
-		data->addr = mlx_get_data_addr(data->img,  &data->bits_per_pixel, &data->line_length, &data->endian);
-		texture.addr = mlx_get_data_addr(texture.img,  &texture.bits_per_pixel, &texture.line_length, &texture.endian);
-		y = -1;
-		while (++y < 64)
-		{
-			x = -1;
-			while (++x < 64)
-			{
-				buf = texture.addr + (y * texture.line_length + x * (texture.bits_per_pixel / 8));
-				my_mlx_pixel_put(data, x, y, *(unsigned int *)buf);
-			}
-		}
-		vars->imgs[i] = (char *)data->img;
-		i++;
-	}
-	
-	
-}
+
 
 int	main(void)
 {
@@ -631,7 +662,7 @@ int	main(void)
 	vars->isRayFacingUp = 0;
 	vars->player_lx  = 0 ;
 	vars->Wall_strip_width = 1;
-	vars->num_rays = (64 * 33 ) / vars->Wall_strip_width;
+	vars->num_rays = (64 * 33);
 	// vars.stepsy = 0;
 	// vars->rays_list = malloc(vars.num_rays * sizeof(int));
 	vars->keys = malloc(sizeof(t_keys));
@@ -647,10 +678,10 @@ int	main(void)
 	vars->data = malloc(sizeof(t_data));
 	int height;
 	int width;
-	vars->img_E = "./path_to_the_east_texture.xpm";
-	vars->img_W = "./path_to_the_west_texture.xpm";
-	vars->img_S = "./path_to_the_south_texture.xpm";
-	vars->img_N = "./path_to_the_north_texture.xpm";
+	vars->img_E = "./wall_N.xpm";
+	// vars->img_W = "./path_to_the_west_texture.xpm";
+	// vars->img_S = "./path_to_the_south_texture.xpm";
+	// vars->img_N = "./path_to_the_north_texture.xpm";
 	
 if (parcer_map(vars) == 0) 
 {
@@ -658,25 +689,16 @@ if (parcer_map(vars) == 0)
 	return 0;
 }
 	vars->mlx = mlx_init();
-	vars->texture_N = mlx_xpm_file_to_image(vars->mlx,vars->img_N, &width, &height);
-	vars->texture_E = mlx_xpm_file_to_image(vars->mlx,vars->img_E, &width, &height);
-	vars->texture_S = mlx_xpm_file_to_image(vars->mlx,vars->img_S, &width, &height);
-	vars->texture_W = mlx_xpm_file_to_image(vars->mlx,vars->img_W, &width, &height);
-	if(vars->texture_W == NULL)
+	vars->texture_N = mlx_xpm_file_to_image(vars->mlx,vars->img_E, &width, &height);
+	// vars->texture_E = mlx_xpm_file_to_image(vars->mlx,vars->img_E, &width, &height);
+	// vars->texture_S = mlx_xpm_file_to_image(vars->mlx,vars->img_S, &width, &height);
+	// vars->texture_W = mlx_xpm_file_to_image(vars->mlx,vars->img_W, &width, &height);
+	if(vars->texture_N == NULL)
 	{
-		puts("here");
 		exit(0);
 	}
-	
-	// fill_buffer2(vars, data);
 	vars->mlx_win = mlx_new_window(vars->mlx, ( 33 * 64) , (14 * 64), "unrecorded");
-vars->data->img = mlx_new_image(vars->mlx, (33 *64), (14 * 64));
-	vars->data->addr = mlx_get_data_addr(vars->data->img,  &vars->data->bits_per_pixel, &vars->data->line_length, &vars->data->endian);
-	// // my_mlx_pixel_put(data, 5, 5, 0xEEEF0000);
-	
-	//   ft_draw(&vars);
-	
-	// mlx_destroy_window(vars.mlx,vars.mlx_win);
+
 	int i = 0;
 	int j = 0;
 	while(vars->store_map[i])
@@ -701,8 +723,7 @@ vars->data->img = mlx_new_image(vars->mlx, (33 *64), (14 * 64));
 	mlx_hook(vars->mlx_win,3,  0, &key_released, vars);
 
 	mlx_loop_hook(vars->mlx, ft_draw, vars);
-	// mlx_loop_hook(vars.mlx, ft_draw_projection, &vars);
-	// mlx_key_hook(vars.mlx_win, key_hook, &vars);
+
 	
 	mlx_loop(vars->mlx);
     
